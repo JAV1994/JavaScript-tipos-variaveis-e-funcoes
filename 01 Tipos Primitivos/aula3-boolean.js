6b8511335649435e49c62f3fcefd7ecbcfb7aedf
JAV1994 <jose.valim@escola.pr.gov.br>
2026-02-11 11:14:44 -0300

    feat: Aula 3 Tipos Booleanos

---
+++ 01 Tipos Primitivos/aula3-boolean.js
@@ -0,0 +1,19 @@
+// Boolean: Usados para tomada de decisão.
+// True: Verdadeiro.
+// False: falso.
+
+const numero1 = 1;
+const numero2 = 2;
+
+
+// == Atribuição Matemática
+// === Comparação retorna verdadeiro e falso;
+console.log("Numero 1 é igual em tipo e valor com o numero 2: " + numero1 === numero2)
+
+
+// let permite a mudança da variavel e é mais segura que var
+let teste = false;
+teste = true;
+console.log(teste)
+
+
