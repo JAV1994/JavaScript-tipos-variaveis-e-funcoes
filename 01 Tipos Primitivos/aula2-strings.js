be931dd71ba7270cca5f19342447d83e07d04b9a
JAV1994 <jose.valim@escola.pr.gov.br>
2026-02-11 10:50:24 -0300

    feat: Aula 2 - Tipo String

---
+++ 01 Tipos Primitivos/aula2-strings.js
@@ -0,0 +1,23 @@
+// Caracter: letra ou simbolo.
+// Strings: Conjunto de caracteres.
+// Javascript trata caracter como string de tamanho 1.
+
+const texto1 = "Hello, World!";
+const texto2 = 'Hello, World!';
+
+const exemplo = "SenhaSuperSegura";
+
+// String de numeros: Conjunto caracteres numerais.
+// Sem propriedades matemáticas.
+const stringNumeros = "12345";
+
+// Template Literal: 3º forma de se escrever strings.
+
+// Concatenar: Juntar mais de uma string.
+const nome = "Jose";
+const sobrenome = "Valim";
+const nomeInteiro = nome + sobrenome;
+
+console.log("Nome Inteiro: " + nomeInteiro)
+
+
